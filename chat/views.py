e51bc5602804416e3f252796b02aecf9d59fa1fc
FroggyRiseman <froggyriseman@gmail.com>
2015-06-23 11:11:34 +0200

    Created a seperate app for userprofile - plan on doing a bit more stuff with user profiles (customization, accessing other user's profile for viewing, etc.).

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.models import User
 
 def index(request):
     return render(request, 'chat/index.html')
@@ -11,5 +10,8 @@ def about(request):
     return render(request, 'chat/about.html')
 
 
+@login_required
 def roster(request):
-    return render(request, 'chat/roster.html')
+    roster_list = User.objects.order_by('username')
+    context_dict = {'members': roster_list}
+    return render(request, 'chat/roster.html', context_dict)

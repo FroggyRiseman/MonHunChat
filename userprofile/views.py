5e344148d53f269c1d16ef9e775a1c7a500f828f
FroggyRiseman <froggyriseman@gmail.com>
2015-07-04 15:00:39 +0200

    Added the roster list with links to each users profile page

--- userprofile/views.py
+++ userprofile/views.py
@@ -10,7 +10,7 @@ from userprofile.models import UserProfile
 from forms import UserProfileForm
 
 def roster(request):
-    roster_list = User.objects.order_by('username')
+    roster_list = UserProfile.objects.order_by('user')
     context_dict = {'members': roster_list}
     return render(request, 'userprofile/roster.html', context_dict)
 
@@ -53,8 +53,8 @@ def profile(request, profile_id):
     else:
         userProfile = UserProfile.objects.get(pk=profile_id)
 
-        return render_to_response(
-                'userprofile/profile.html',
-                {'userProfile': userProfile},
-                RequestContext(request)
-        )
+    return render_to_response(
+            'userprofile/profile.html',
+            {'userProfile': userProfile},
+            RequestContext(request)
+    )
